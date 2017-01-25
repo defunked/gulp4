6ca386a1520a586f10b16c795df2419e9270f3f6
A.Greegoriev <a.greegoriev@gmail.com>
2017-01-25 18:20:18 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const gulp = require('gulp');
+const stylus = require('gulp-stylus');
+
+gulp.task('styles', function() {
+  return gulp.src('frontend/styles/**/*.styl')
+    .pipe(stylus())
+    .pipe(gulp.dest('public'));
+});
